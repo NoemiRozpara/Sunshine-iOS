079260d65b1126ec2c6dfa5721e3be317183bef3
Noemi Rozpara <nrozpara@gmail.com>
2020-10-29 14:49:34 +0100

    Initial Commit

---
+++ Sunshine/SunshineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunshineApp.swift
+//  Sunshine
+//
+//  Created by Noemi on 29/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SunshineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
